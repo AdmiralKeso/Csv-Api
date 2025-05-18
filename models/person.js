1acba19c4613431cc3f9c090bf45c20c28133d6f
AdmiralKeso <cas.hagenaar@gmail.com>
2025-05-18 21:01:47 +0200

    Add: Person template

---
+++ models/person.js
@@ -0,0 +1,11 @@
+// Template model for Person
+class Person {
+  constructor(id, name, age, email) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+}
+// Exports it as Person
+module.exports = Person;
